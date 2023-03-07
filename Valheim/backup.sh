1634b268deb5103b36b267601b07aa321536b9db
Arvind Sagar <arvindsagar702@gmail.com>
2023-03-08 04:47:05 +0530

    Update backup.sh

--- Valheim/backup.sh
+++ Valheim/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BACKUP_FILE=$1
-TOKEN=$2
+FOLDERID=$2
+TOKEN=$3
 
 response=$(curl -s https://api.gofile.io/getServer)
 
@@ -8,7 +9,7 @@ response=$(curl -s https://api.gofile.io/getServer)
 server=$(echo $response | jq -r '.data.server')
 
 # Concatenate the server value with the URL
-url= "https://"$server".gofile.io/uploadFile"
+url="https://$server.gofile.io/uploadFile"
 
 # Make the second request using the full URL
-curl -F file=@$1 -F "folderId=bbde88d5-dd53-4a2e-955a-6457701037d8" -F "token=$2" 
+curl -F file=@$BACKUP_FILE -F "folderId=$FOLDERID" -F "token=$TOKEN" "$url"
